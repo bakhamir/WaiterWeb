bb8ac5e7eb8ce588f4c5b784e96f454fcd23fba5
БахытжановА <@207-3>
2024-02-09 21:40:11 +0600

    Добавьте файлы проекта.

---
+++ WaiterWeb/Controllers/WaiterController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Dapper;
+using WaiterWeb.Models;
+namespace WaiterWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WaiterController : ControllerBase
+    {
+        [HttpPost("/LoginWaiter")]
+        public async Task<string> LoginWaiter(int id)
+        {
+            string conStr = @"Data Source=207-3;Initial Catalog=WaiterDb;Integrated Security=True;TrustServerCertificate=Yes";
+            try
+            {
+                using (var db = new SqlConnection(conStr))
+                {
+                    DynamicParameters p = new DynamicParameters();
+                    p.Add("@id", id);
+                    string res = db.ExecuteScalar("pWaiter",p, commandType: System.Data.CommandType.StoredProcedure).ToString();
+                    return res;    
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Error broken id or smth";
+            }
+        }
+        [HttpPost("/GetTables")]
+        public async Task<IEnumerable<diningtable>> GetTables(int id)
+        {
+            string conStr = @"Data Source=207-3;Initial Catalog=WaiterDb;Integrated Security=True;TrustServerCertificate=Yes";
+            try
+            {
+                using (var db = new SqlConnection(conStr))
+                {
+                    DynamicParameters p = new DynamicParameters();
+                    p.Add("@id", id);
+                    var res = db.Query<diningtable>("pTable",p, commandType: System.Data.CommandType.StoredProcedure);
+                    return res;    
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ WaiterWeb/Models/Waiter.cs
@@ -0,0 +1,31 @@
+﻿namespace WaiterWeb.Models
+{
+	public class waiter
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+
+	}
+	public class diningtable
+	{
+		public int id { get; set; }
+		public int waiterid { get; set; }
+	}
+	public class dishes
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public int price { get; set; }
+
+	}
+	public class orders
+	{
+		public int id { get; set; }
+		public bool status { get; set; }
+		public int waiterid { get; set; }
+
+	}
+
+
+
+}
